304e5415585418db7cacd034f4f6b745e784d221
Draven <jspankai@vip.qq.com>
2017-08-13 12:03:40 +0800

    add class Product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product'
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DGList';
-  product = {
+  product: Product = {
     title: '苹果Apple iPhone7 手机 黑色 32GB',
     price: 4498.00,
     image: 'http://img14.360buyimg.com/n1/s450x450_jfs/t3754/265/902031417/103522/d308968f/581af17fN1057992a.jpg'
