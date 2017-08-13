ca961ba30754661711b9dc01777e1cc9e532877e
Draven <jspankai@vip.qq.com>
2017-08-13 11:30:25 +0800

    display a product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DGList';
+  product = {
+    title: '苹果Apple iPhone7 手机 黑色 32GB',
+    price: 4498.00,
+    image: 'http://img14.360buyimg.com/n1/s450x450_jfs/t3754/265/902031417/103522/d308968f/581af17fN1057992a.jpg'
+  };
 }
